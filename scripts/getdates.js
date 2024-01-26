4c2eabc1e8eccb7881b90d8a8e458538583159c9
Dylan Petersen <pete1048310@byui.edu>
2024-01-26 13:30:08 -0700

    added an hamburger menu for my navigation menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,4 +6,11 @@ document.getElementById('currentYear').innerText = new Date().getFullYear();
 
 // Dynamically populate the date the document was last modified in the second paragraph
 document.getElementById('lastModified').innerText = "Last modified: " + document.lastModified;
-document.getElementById("lastModified").innerText = "Last modified: " + lastModifiedTimestamp;
+// document.getElementById("lastModified").innerText = "Last modified: " + lastModifiedTimestamp;
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
