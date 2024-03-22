f8f41c6e3775d8bf72b3037aba5b709b7d23003c
Dylan Petersen <pete1048310@byui.edu>
2024-03-22 13:36:07 -0600

    updated my weather section on my home page

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,12 +1,12 @@
 const apiKey = 'e35c72d7b6d177a12d1670b7d11a79f6';
-const chamberLocation = {
-    city: 'Las Vegas',
-    countryCode: 'US' // If applicable
-};
+// const chamberLocation = {
+//     city: 'Las Vegas',
+//     countryCode: 'US' // If applicable
+// };
 
 // Function to fetch weather data
 async function fetchWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${chamberLocation.city},${chamberLocation.countryCode}&units=metric&appid=${apiKey}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=36.17187151307037&lon=-115.14330849154501&units=metric&appid=${apiKey}`);
     const data = await response.json();
     const currentTemp = data.main.temp;
     const weatherDescription = data.weather[0].description;
@@ -15,7 +15,7 @@ async function fetchWeather() {
     document.getElementById('weather-description').textContent = `Weather Description: ${weatherDescription}`;
 
     // Fetch three-day forecast
-    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${chamberLocation.city},${chamberLocation.countryCode}&units=metric&appid=${apiKey}`);
+    const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=36.17187151307037&lon=-115.14330849154501&units=metric&appid=${apiKey}`);
     const forecastData = await forecastResponse.json();
     const forecastList = document.getElementById('forecast-list');
     forecastList.innerHTML = ''; // Clear previous forecast
